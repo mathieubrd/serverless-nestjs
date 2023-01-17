c834d64d2ae18d987290b1a42fddd7e168b166c8
Mathieu Brochard <mathieu.brochard@outlook.com>
2023-01-17 12:23:52 +0100

    Remove fixtures script

--- fixtures.ts
+++
@@ -1,8 +0,0 @@
-import Application from "./src/applications/application.model"
-import { faker } from "@faker-js/faker"
-
-const applications = (amount: number) => {
-  const applications = [...Array(amount).keys()].map(() => {
-    const application = new Application()
-  })
-}
